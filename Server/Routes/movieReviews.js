90a70c4246b93f48403202f59046e8341d70c0ac
vignan83 <saivignanreddy0@gmail.com>
2022-05-06 19:50:00 +0530

    latest commit

--- Server/Routes/movieReviews.js
+++ Server/Routes/movieReviews.js
@@ -4,41 +4,39 @@ const Movie = require('../Models/Movie');
 const auth = require('../middlewares/auth');
 const mongoose = require('mongoose');
 
-router.put("/:movieid",async (req,res)=>{
-    const {movieid} = req.params;
-    const {rating,review} = req.body;
+router.put("/:movieid", async (req, res) => {
+    const { movieid } = req.params;
+    const { rating, review } = req.body;
     const userId = req.session.userId;
-    try{
-        Movie.findOne({_id:movieid}).then(movie=>{
-        const exisitingUser =  movie.reviews.some((review) => review.userId == userId);
-        if(!exisitingUser){
-            console.log("reviews",movie.reviews);      
-            movie.reviews.push({userId,rating:rating,review:review})
-            movie.save().then(()=>{
-                res.send(movie);
-            })
-        } 
-         else{
-             Movie.updateOne({
-                "reviews.userId":userId
-            },
-            {
-               $set:
-                   {
-                   "reviews.$.rating":rating,
-                   "reviews.$.review":review
-               }
-            }).then(() => {
-                        res.status(204).send();
+    try {
+        Movie.findOne({ _id: movieid }).then(movie => {
+            const exisitingUser = movie.reviews.some((review) => review.userId == userId);
+            if (!exisitingUser) {
+                movie.reviews.push({ userId, rating: rating, review: review })
+                movie.save().then(() => {
+                    res.send(movie);
+                })
+            }
+            else {
+                Movie.updateOne({
+                    "reviews.userId": userId
+                },
+                    {
+                        $set:
+                        {
+                            "reviews.$.rating": rating,
+                            "reviews.$.review": review
+                        }
+                    }).then(() => {
+                        res.status(204).send(Movie);
                     }).catch((e) => {
-                        console.log(e.message);
                         res.status(500).send({ error: "Internal Server Error" });
-            });
-         }
+                    });
+            }
 
         })
-}
-    catch(e){
+    }
+    catch (e) {
         console.log(e.message);
     }
 
