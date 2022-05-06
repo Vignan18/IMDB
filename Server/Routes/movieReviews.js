95a1b06ea23170a883cfb026c22876b6d3eac694
vignan83 <saivignanreddy0@gmail.com>
2022-05-06 18:29:21 +0530

    latest commit

--- Server/Routes/movieReviews.js
+++ Server/Routes/movieReviews.js
@@ -2,61 +2,41 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../Models/Movie');
 const auth = require('../middlewares/auth');
+const mongoose = require('mongoose');
 
 router.put("/:movieid",async (req,res)=>{
     const {movieid} = req.params;
-    const {reviews} = req.body;
+    const {rating,review} = req.body;
     const userId = req.session.userId;
-    console.log("userId",userId);
     try{
-        const movieData = await Movie.findById(movieid);
-        const exisitingUser =  movieData.reviews.some((review) => review.userId == userId);
-        console.log("exisiting user",exisitingUser);
+        Movie.findOne({_id:movieid}).then(movie=>{
+        const exisitingUser =  movie.reviews.some((review) => review.userId == userId);
         if(!exisitingUser){
-            console.log("reviews",movieData.reviews);
-           await movieData.reviews.push({
-               userId,
-               rating:reviews.rating,
-                review:reviews.review
-           })
-           res.status(204).send();
-       
+            console.log("reviews",movie.reviews);      
+            movie.reviews.push({userId,rating:rating,review:review})
+            movie.save().then(()=>{
+                res.send(movie);
+            })
         } 
-        // // User already given review and rating
          else{
-             //loop a particular user 3
-             //id -> 
-
-
-             let userIndex = movieData.reviews.findIndex((a) => a.userId == userId);
-             console.log(movieData.reviews[userIndex]);
-            await movieData.updateOne({
-                 "reviews.userId":userId
-             },
-
-             
-             {
-                $set:{
-                    "reviews.$.userId":userId,
-                    "reviews.$.rating":reviews.rating,
-                    "reviews.$.review":reviews.review
-                }
-             }).then(() => {
+             Movie.updateOne({
+                "reviews.userId":userId
+            },
+            {
+               $set:
+                   {
+                   "reviews.$.rating":rating,
+                   "reviews.$.review":review
+               }
+            }).then(() => {
                         res.status(204).send();
-                    }).catch(() => {
+                    }).catch((e) => {
+                        console.log(e.message);
                         res.status(500).send({ error: "Internal Server Error" });
             });
-             
-         //  console.log(movieData.findOne({"reviews.userid":userId}))
-        //    await movieData.updateOne({ "reviews.userId" : userId }, {$set:{reviews:reviews}}).then(() => {
-        //         res.status(204).send();
-        //     }).catch(() => {
-        //         res.status(500).send({ error: "Internal Server Error" });
-        //     });
          }
-        
-        
-    // }
+
+        })
 }
     catch(e){
         console.log(e.message);
